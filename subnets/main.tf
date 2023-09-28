9fd5d9b7bdbe7c338a24e0ec54d41ddc5fcee0fd
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-28 14:39:16 +0100

    added the prometheus ec2

--- subnets/main.tf
+++ subnets/main.tf
@@ -2,8 +2,8 @@ resource "aws_subnet" "main" {
   count             = length(var.cidr_block)
   vpc_id            = var.vpc_id
   cidr_block        = element(var.cidr_block, count.index)
-  availability_zone = elememt(var.az, count.index)
-   tag = merge({
+  availability_zone = element(var.az, count.index)
+   tags = merge({
     Name = "${var.env}-${var.subnet_name}-subnet"
   },
     var.tags["default"])
